7ba87d81dc5ae2e7dfde5fbc5a98a9ae94799283
bach <byebachgm@gmail.com>
2022-07-04 16:58:39 -0300

    Showcase agregado

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 export const Showcase = () => {
   return (
     <Section className='showcase'>
+        
         <h2>Que es leete la vida</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quasi! Ipsa esse enim velit modi eveniet, ex similique mollitia necessitatibus odio! Cum officia iste qui laboriosam soluta rerum explicabo vitae!</p>
         <a href="/">
@@ -17,5 +18,5 @@ export const Showcase = () => {
 export default Showcase;
 
 const Section = styled.section`
-        
+
 `
