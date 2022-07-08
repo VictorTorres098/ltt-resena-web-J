f6ea222ab0835db22c7225b79eb5ac9192a94980
victor <victortorres098@gmail.com>
2022-07-08 20:06:19 -0300

    nada

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ButtonReact from '../button/Button';
 import styled from 'styled-components';
+import imgShowcase from '../../assets/img/showcaseImg.jpg'
 
 export const Showcase = () => {
   return (
     <Section className='showcase'>
-        
-        <h2>Que es leete la vida</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quasi! Ipsa esse enim velit modi eveniet, ex similique mollitia necessitatibus odio! Cum officia iste qui laboriosam soluta rerum explicabo vitae!</p>
-        <a href="/">
-            <ButtonReact buttonText="Quiero saber mas!"/>
-        </a>
+        <div className='showcaseInfo'>
+          <h2>Que es leete la vida</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quasi! Ipsa esse enim velit modi eveniet, ex similique mollitia necessitatibus odio! Cum officia iste qui laboriosam soluta rerum explicabo vitae!</p>
+          <a href="/">
+              <ButtonReact buttonText="Quiero saber mas!"/>
+          </a>
+        </div> 
     </Section>
   )
 }
@@ -18,5 +20,13 @@ export const Showcase = () => {
 export default Showcase;
 
 const Section = styled.section`
-
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #fdcae1;
+  height: 700px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
